26a184cb6192ad3392de6748201cb2f8e45f0c72
leandrolopesr <rafalopes580@gmail.com>
2023-08-19 15:31:58 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frutas';
+
+  title = 'Lista de Tarefas';
+
+  public tarefa = "";
+  public items = [""];
+
+
+add(){
+  if(this.tarefa == ""){
+    window.alert("Digite uma Tarefa Valida!")
+  }
+  else{
+    this.items.push(this.tarefa);
+  }
+  
+}
+
+remove(item: string){
+  this.items.splice(this.items.indexOf(item), 1);
+}
+  
 }
